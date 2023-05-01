80571d8e2ca6820c39c04ae39fa68f94326cb232
SRS1 <saraswan1999@gmail.com>
2023-05-01 12:50:19 +0530

    Featured and Category
    Category ViewSet and update API for updating the featured

--- LittleLemon/LittleLemonAPI/urls.py
+++ LittleLemon/LittleLemonAPI/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CartViewSet, ManagerViewSet, DeliveryCrewViewSet, MenuItemViewSet, CustomerView, OrderViewSet
+from .views import (CartViewSet, 
+                    ManagerViewSet, 
+                    DeliveryCrewViewSet, 
+                    MenuItemViewSet, 
+                    CustomerView, 
+                    OrderViewSet, 
+                    CategoryViewSet, 
+                    SetFeaturedView)
 
 
 router = DefaultRouter(trailing_slash = False)
@@ -9,9 +16,11 @@ router.register('groups/managers/users', viewset=ManagerViewSet, basename='manag
 router.register('groups/delivery-crew/users', viewset=DeliveryCrewViewSet, basename='delivery-crew-view')
 router.register('cart/menu-items', viewset=CartViewSet, basename='cart-view')
 router.register('orders', viewset=OrderViewSet, basename='order-view')
+router.register('category', viewset=CategoryViewSet, basename='category-view')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('groups/customer/users', CustomerView.as_view()),
+    path('set-featured/<int:pk>', SetFeaturedView.as_view()),
     path('', include('djoser.urls')),
 ]
